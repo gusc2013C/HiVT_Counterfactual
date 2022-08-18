04f81ea8a152558b343d158476c6dfbaa6a10909
Desperado <849843439@qq.com>
2022-08-18 17:58:19 +0800

    Fixed

--- new_vis.py
+++ new_vis.py
@@ -1,3 +1,6 @@
 from argoverse.visualization.visualize_sequences import viz_sequence
-seq_path = f"/home/be-happy/GSC/forecasting_test_v1.1/val/data/1.csv"
+
+root_dir = '/home/be-happy/GSC/forecasting_test_v1.1/val/data/'
+afl = ArgoverseForecastingLoader(root_dir)
+seq_path = f"{root_dir}/1.csv"
 viz_sequence(afl.get(seq_path).seq_df, show=True)
